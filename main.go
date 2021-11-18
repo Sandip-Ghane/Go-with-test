a2e9c6e879fc44affd196db83fd06729015e22e9
Sandip-Ghane <sandip@workshiphq.com>
2021-11-18 20:47:58 +0530

    added multiple sub testcases

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello "
+const spanishPrefix = "Hola "
+const frenchPrefix = "Bonjour "
 
 // Hello ...
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		return englishPrefix + "world"
+	}
+	if language == "spanish" {
+		return spanishPrefix + name
+	}
+	if language == "french" {
+		return frenchPrefix + name
 	}
 	return englishPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("sandip"))
+	fmt.Println(Hello("sandip", ""))
 }
--- main_test.go
+++ main_test.go
@@ -10,13 +10,23 @@ func TestMain(t *testing.T) {
 		}
 	}
 	t.Run("test to hello people", func(t *testing.T) {
-		got := Hello("Sandip")
+		got := Hello("Sandip", "")
 		want := "Hello Sandip"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("test to empty people", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("spanish", func(t *testing.T) {
+		got := Hello("Sandip", "spanish")
+		want := "Hola Sandip"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("french", func(t *testing.T) {
+		got := Hello("Sandip", "french")
+		want := "Bonjour Sandip"
 		assertCorrectMessage(t, got, want)
 	})
 }
