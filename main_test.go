6f4f078cc1abd947d4d1ad2e6154d9b3defbe3e7
Sandip-Ghane <sandip@workshiphq.com>
2021-11-18 20:34:27 +0530

    refactor test cases

--- main_test.go
+++ main_test.go
@@ -3,18 +3,20 @@ package main
 import "testing"
 
 func TestMain(t *testing.T) {
-	t.Run("test to hello people", func(t *testing.T) {
-		got := Hello("sandip")
-		want := "Hello sandip"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 		if got != want {
-			t.Errorf("Expect: %s but got: %s", want, got)
+			t.Errorf("Expected: %q but want: %q", got, want)
 		}
+	}
+	t.Run("test to hello people", func(t *testing.T) {
+		got := Hello("Sandip")
+		want := "Hello Sandip"
+		assertCorrectMessage(t, got, want)
 	})
-	t.Run("test to empty people name", func(t *testing.T) {
+	t.Run("test to empty people", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello world"
-		if got != want {
-			t.Errorf("Expect: %s but got: %s", want, got)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
